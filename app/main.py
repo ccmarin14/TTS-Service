96f735e3d59b791e2a0bfded749a030220af59e9
Cristian Camilo Marin Florez <cristianmarin.gma@gmail.com>
2024-09-26 14:33:01 -0500

    Cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.tts_controller import router as tts_router
 
 app = FastAPI()
+
+# Configuración de CORS
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
+app.mount("/app/resources", StaticFiles(directory="app/resources"), name="resources")
 
 app.include_router(tts_router)
