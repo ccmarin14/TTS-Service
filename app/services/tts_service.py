16fad1e272c5ced292b36ee6e0f30dc8cd2f2b70
Cristian Camilo Marin Florez <cristianmarin.gma@gmail.com>
2024-10-07 09:56:39 -0500

    Ajustes de codificación

--- app/services/tts_service.py
+++ app/services/tts_service.py
@@ -1,10 +1,12 @@
 import json
+import re
 from TTS.api import TTS
+
 
 class TTSService:
     def __init__(self):
         # Cargar los modelos desde el archivo JSON
-        with open("app/data/model.json", "r", encoding="utf-8") as f:
+        with open("app/data/model.json", "r") as f:
             data = json.load(f)
             self.models = {model['id']: model for model in data['models']}
 
@@ -15,11 +17,13 @@ class TTSService:
         
         model_name = model_data['model']
         
-        # Asegurarse de que el texto esté en UTF-8
-        print(repr(text))
-        text = text.encode('utf-8', 'replace').decode('utf-8')
-
         # Inicializar el modelo y generar el audio
+        text = clean_text(text)
         tts = TTS(model_name=model_name)
         tts.tts_to_file(text, file_path=output_path, speed=1.0, pitch=1.0)
 
+
+
+    def clean_text(text):
+        # Elimina caracteres no alfanuméricos excepto espacios
+        return re.sub(r'[^a-zA-Z0-9\s]', '', text)
